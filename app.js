67b530c461ccd2c65cdfc4781425c1866143e072
Michael McCann <81657333+MichaelMcCann1@users.noreply.github.com>
2021-10-04 14:24:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const currentLetter = document.querySelector('#currentLetter')
 const remainingLetters = document.querySelector('#remainingLetters')
 const accuracyPercentage = document.querySelector('#accuracyPercentage')
 const grossWPM = document.querySelector('#grossWPM')
+const body = document.querySelector('body')
+const inputBox = document.querySelector('.inputBox')
+const tapScreen = document.querySelector('.tapScreen')
 
 // Variables
 let startTime;
 let AccuracyCounterWrong = 0;
 let AccurayCounterTotal = 0;
-let GrossWordsCounter = 0;
+let correctLetterCounter = 0;
 let currentWordLength = 1;
 let currentLetterCorrect = true;
 
@@ -17,11 +20,14 @@ let margin = window.getComputedStyle(completedLeters)['margin-left']
 margin = parseInt(margin.substring(0, margin.length - 2))
 let fontWidth = window.getComputedStyle(completedLeters)['font-size'] === '36px'? 21.6 : 11.6
 
-let paragraph = 'The story goes that a man punished his daughter for wasting a roll of gold wrapping paper. Money was tight and he became infuriated when the child tried to decorate a box to put under the Christmas tree. Nevertheless, the little girl brought the gift to her father the next morning and said, "This is for you, Daddy".'
+let paragraph = 'The story goes that a man punished his daughter for wasting a roll of gold wrapping paper. Money was tight and he became infuriated when the child tried to decorate a box to put under the Christmas tree. Nevertheless, the little girl brought the gift to her father the next morning and said, "This is for you, Daddy."'
 currentLetter.textContent = paragraph[0]
 remainingLetters.textContent = paragraph.slice(1)
 
-
+body.addEventListener('click', () => {
+  inputBox.focus()
+  tapScreen.style.display = 'none'
+})
 
 document.addEventListener('keypress', (event) => {
   if (AccurayCounterTotal === 0) { //start timer if it is first keypress on page refresh
@@ -50,15 +56,14 @@ document.addEventListener('keypress', (event) => {
 
     currentWordLength++;
     currentLetterCorrect = true;
-    GrossWordsCounter++;
-    updateGrossWPM(GrossWordsCounter, startTime);
+    correctLetterCounter++;
+    updateGrossWPM(correctLetterCounter, startTime);
     updateAccuracy(AccuracyCounterWrong, AccurayCounterTotal);
 
   } else { //if key is wrong
     currentLetterCorrect = false;
     AccuracyCounterWrong++;
     updateAccuracy(AccuracyCounterWrong, AccurayCounterTotal);
-
   }
 })
 
